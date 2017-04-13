1e8d210dbaf1e17c3ae77fc48b9c2a0e7ac11742
danielecook <danielecook@gmail.com>
2017-04-13 17:06:51 -0500

    closer to getting things to work

--- functions/bcftools/main.py
+++ functions/bcftools/main.py
@@ -7,11 +7,13 @@ from subprocess import Popen, PIPE
 import re
 import json
 import os
-import traceback
+import sys
+import glob
+from cyvcf2 import VCF
+from cStringIO import StringIO
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 
 def get_region(region):
     m = re.match("^([0-9A-Za-z]+):([0-9]+)-([0-9]+)$", region)
@@ -40,27 +42,28 @@ def msg(out, err, event, status=200):
 
 
 def handle(event, context):
-    try:
-        os.chdir("/tmp")
-        if 'body' in event:
-            event = json.loads(event['body'])
-        logger.info(event)
-        if 'region' not in event or 'vcf' not in event:
-            return msg(None, "Must specify vcf and region", event, 400)
+    if 'body' in event:
+        event = json.loads(event['body'])
+    logger.info(event)
+    if 'region' not in event or 'vcf' not in event:
+        return msg(None, "Must specify vcf and region", event, 400)
 
-        chrom, start, end = get_region(event['region'])
-        event['vcf'] = event['vcf'].replace("https://", "http://")
+    chrom, start, end = get_region(event['region'])
+    event['vcf'] = event['vcf'].replace("https://", "http://")
 
-        if start >= end:
-            return msg(None, "Invalid start and end region values", event, 400)
-        if end - start > 1e6:
-            return msg(None, "Maximum region size is 100 kb", event, 400)
+    if start >= end:
+        return msg(None, "Invalid start and end region values", event, 400)
+    if end - start > 1e6:
+        return msg(None, "Maximum region size is 100 kb", event, 400)
 
-        comm = ["/var/task/bcftools", "view", event["vcf"], event['region']]
-        logger.info(' '.join(comm))
-        out, err = Popen(comm, stdout=PIPE, stderr=PIPE).communicate()
-        logger.info(out + " out")
-        logger.info(err + " err")
-        return msg(out, err, 200)
-    except Exception as e:
-        return msg(None, e, event, 400)
+    comm = ["/var/task/bcftools", "view", event["vcf"], event['region']]
+    logger.info(' '.join(comm))
+    proc = Popen(comm, stdout=PIPE, stderr=PIPE)
+    v = VCF(proc.stdout)
+    for record in v:
+        logger.info(record.CHROM)
+    return msg("t", "t", event, 200)
+
+
+
+
