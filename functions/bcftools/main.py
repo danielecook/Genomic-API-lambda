414fafde35410c37a31eb179b5bb7875e0000c2b
Daniel Cook <danielecook@gmail.com>
2017-04-13 08:15:11 -0500

    functioning!

--- functions/bcftools/main.py
+++ functions/bcftools/main.py
@@ -4,39 +4,51 @@ Lambda example with external dependency
 
 import logging
 from subprocess import Popen, PIPE
-import json
 import re
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def msg(out, err, event, status = 200):
-    return {
-                'statusCode': status,
-                'body': {"out": out, "err": err, "event": event},
-                'headers': {
-                    'Content-Type': 'application/json',
-                }
-            }
 
-def handle(event, context):
-    if 'region' not in event or 'vcf' not in event:
-        return msg(None, "Error: must specify VCF and region", event, 400)
-
-    m = re.match("^([0-9A-Za-z]+):([0-9]+)-([0-9]+)$", event['region'])
-    
+def get_region(region):
+    m = re.match("^([0-9A-Za-z]+):([0-9]+)-([0-9]+)$", region)
     if not m:
         return msg(None, "Invalid region", 400)
 
     chrom = m.group(1)
     start = int(m.group(2))
     end = int(m.group(3))
-    #if !(start-end < 1e6 and start .group(3))- end > 0):
-    #    return msg(None, "Invalid start and end region values", 400)
+    return chrom, start, end
 
-    
-    logger.info("%s", event['body'])
-    out, err = Popen(["./bcftools"], stdout = PIPE, stderr = PIPE).communicate()
+def msg(out, err, event, status=200):
+    if out == "":
+        out = None
+    if err == "":
+        err = None
+    if err.startswith("[E::hts_open]"):
+        err = err.strip()
+    return {
+        'statusCode': status,
+        'body': {"out": out, "err": err, "event": event},
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
+
+
+def handle(event, context):
+    if 'region' not in event or 'vcf' not in event:
+        return msg(None, "Must specify vcf and region", event, 400)
+
+    chrom, start, end = get_region(event['region'])
+    event['vcf'] = event['vcf'].replace("https://", "http://")
+
+    if start >= end:
+        return msg(None, "Invalid start and end region values", event, 400)
+    if end - start > 1e6:
+        return msg(None, "Maximum region size is 100 kb", event, 400)
+
+    out, err = Popen(["./bcftools", "view", event["vcf"]], stdout=PIPE, stderr=PIPE).communicate()
     logger.info(out + " out")
     logger.info(err + " err")
 
